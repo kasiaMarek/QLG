03b72cf7909912e42a4e15b6e94823c98c6e1e08
kasiaMarek <kasia@marek.net>
2019-06-28 23:52:57 +0200

    better fredkin gate

--- src/main/scala/lib/Gates/FredkinGate.scala
+++ src/main/scala/lib/Gates/FredkinGate.scala
@@ -5,15 +5,24 @@ import breeze.numerics.pow
 import lib.Prefs.QNum
 
 class FredkinGate(val control:Int, val swap:(Int,Int)) extends CircuitGate(FredkinGate.getGate) {
+
   override def expand: Int => AnyGate = (size: Int) => {
-    val c1 = pow(2, size - control - 1)
-    val s1 = pow(2, size - swap._1 - 1)
-    val s2 = pow(2, size - swap._2 - 1)
-    val sumSize = pow(2,size)
-    val list = (for(i <- 0 until sumSize if (i/c1) % 2 == 1 && (i/s1) % 2 == 0 && (i/s2) % 2 == 0) yield (i + s1, i + s2)).toList
-    AnyGate(FredkinGate.getGate(list, sumSize))
+    val unsortedListOfVal = List(size - control - 1, size - swap._1 - 1, size - swap._2 - 1)
+    val listOfVal = unsortedListOfVal.sorted
+    val list: List[(Int,Int)] = (for(
+      p1 <- 0 until 1 << listOfVal(0);
+      p2 <- 0 until 1 << (listOfVal(1) - listOfVal(0) - 1);
+      p3 <- 0 until 1 << (listOfVal(2) - listOfVal(1) - 1);
+      p4 <- 0 until 1 << (size - listOfVal(2) - 1)
+    ) yield {
+      val i = p1 | (p2 << (listOfVal(0) + 1)) | (p3 << (listOfVal(1) + 1)) | (p4 << (listOfVal(2) + 1))
+      (
+        i | (1 << unsortedListOfVal.head) | (1 << unsortedListOfVal(1)),
+        i | (1 << unsortedListOfVal.head) | (1 << unsortedListOfVal(2))
+      )
+    }).toList
+    AnyGate(FredkinGate.getGate(list, 1 << size))
   }
-//  override def expand: Int => AnyGate = normalExpand(3,control)
 }
 
 object FredkinGate {
