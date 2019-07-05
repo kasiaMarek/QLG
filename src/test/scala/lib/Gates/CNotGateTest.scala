504106b8786cf17686218477fc7e6e62cdaa4853
kasiaMarek <kasia@marek.net>
2019-07-05 13:23:50 +0200

    cnot gate test

--- src/test/scala/lib/Gates/CNotGateTest.scala
+++ src/test/scala/lib/Gates/CNotGateTest.scala
@@ -1,5 +1,11 @@
 package lib.Gates
 
-class CNotGateTest {
+import lib.Qbits.Qbits
+import org.scalatest.FunSuite
+
+class CNotGateTest extends FunSuite {
+  test("*") {
+    assert(new CNotGate(2,0) * new Qbits(1,1,1,0) == new Qbits(0,1,1,0))
+  }
 
 }
